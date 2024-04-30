16190429001601a7a6fefb55abf8559483d41ce5
Kei-K23 <keiksl2301@gmail.com>
2024-04-30 18:11:36 +0630

    Add challenge page header

---
+++ src/app/(challenges)/_components/header.tsx
@@ -0,0 +1,36 @@
+import { Progress } from "@/components/ui/progress";
+import { InfinityIcon, X } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+type HeaderProps = {
+  hearts: number;
+  percentage: number;
+};
+
+const Header = ({ hearts, percentage }: HeaderProps) => {
+  //   const { open } = useExistModal();
+
+  return (
+    <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1150px] w-full mx-auto">
+      <X
+        // onClick={open}
+        className="text-neutral-500 hover:opacity-75 transition cursor-pointer w-8 h-8"
+      />
+      <Progress value={percentage} />
+
+      <div className="text-rose-500 flex items-center font-bold">
+        <Image
+          src={"/heart.svg"}
+          alt="heart icon"
+          height={28}
+          width={28}
+          className="mr-2"
+        />
+        {hearts}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/(challenges)/_components/main.tsx
@@ -0,0 +1,162 @@
+"use client";
+
+import React, { useState, useTransition } from "react";
+import Footer from "./footer";
+
+import { toast } from "sonner";
+import { useAudio, useMount } from "react-use";
+import { ChallengeWithChallengeProgress } from "@/types";
+import Header from "./header";
+
+type MainProps = {
+  initialPercentage: number;
+  initialChallengeWithChallengeProgress: ChallengeWithChallengeProgress;
+  initialHeart: number;
+};
+
+const Main = ({
+  initialHeart,
+  initialChallengeWithChallengeProgress,
+  initialPercentage,
+}: MainProps) => {
+  const [correctAudio, _correct, correctControl] = useAudio({
+    src: "/correct.wav",
+  });
+  const [incorrectAudio, _incorrect, incorrectControl] = useAudio({
+    src: "incorrect.wav",
+  });
+  const [pending, startTransition] = useTransition();
+  const [hearts, setHearts] = useState(initialHeart);
+  const [percentage, setPercentage] = useState(() =>
+    initialPercentage === 100 ? 0 : initialPercentage
+  );
+
+  const [challenge] = useState(initialChallengeWithChallengeProgress);
+
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"none" | "correct" | "incorrect">(
+    "none"
+  );
+
+  const { open: OpenNoEnoughHeartsModal } = useNoEnoughHeartsModal();
+
+  useMount(() => {
+    if (initialPercentage === 100) {
+      OpenPracticeModal();
+    }
+  });
+
+  const options = challenge?.challengeOptions ?? [];
+
+  if (!challenge) {
+    // TODO: Finish screen
+    return <div>finish screen</div>;
+  }
+
+  const title =
+    challenge.type === "MULTIPLE_CHOICE"
+      ? "Select the correct meaning"
+      : challenge.question;
+
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+    setSelectedOption(id);
+  };
+
+  const onContinue = () => {
+    if (!selectedOption) return;
+
+    if (status === "incorrect") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === "correct") {
+      onNext();
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    const correctOption = options.find((option) => option.correct);
+
+    if (!correctOption) return;
+
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id)
+          .then((res) => {
+            if (res?.error === "hearts") {
+              OpenNoEnoughHeartsModal();
+              return;
+            }
+
+            correctControl.play();
+            setStatus("correct");
+            setPercentage((prev) => prev + 100 / challenges.length);
+
+            if (initialPercentage === 100) {
+              setHearts((prev) => Math.min(prev + 1, 5));
+            }
+          })
+          .catch(() => toast.error("Something went wrong. Please try again!"));
+      });
+    } else {
+      startTransition(() => {
+        reduceHeart(challenge.id)
+          .then((res) => {
+            if (res?.error === "hearts") {
+              OpenNoEnoughHeartsModal();
+              return;
+            }
+            incorrectControl.play();
+            setStatus("incorrect");
+            if (!res?.error) {
+              setHearts((prev) => Math.max(prev - 1, 0));
+            }
+          })
+          .catch(() => toast.error("Something went wrong. Please try again!"));
+      });
+    }
+  };
+
+  return (
+    <>
+      {correctAudio}
+      {incorrectAudio}
+      <Header hearts={hearts} percentage={percentage} />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={pending}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer
+        onCheck={onContinue}
+        status={status}
+        disabled={pending || !selectedOption}
+        lessonId={initialLessonId}
+      />
+    </>
+  );
+};
+
+export default Main;
