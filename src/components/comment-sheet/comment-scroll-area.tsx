fdcf2f0779d7e70671fc16db9ffcc23b31e96d56
Kei-K23 <keiksl2301@gmail.com>
2024-05-06 17:43:56 +0630

    Add auto scroll to comment section

--- src/components/comment-sheet/comment-scroll-area.tsx
+++ src/components/comment-sheet/comment-scroll-area.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ScrollArea } from "../ui/scroll-area";
 import { FirebaseCommentDocType } from "@/types";
 import {
@@ -19,6 +19,7 @@ type CommentScrollAreaProps = {
 
 const CommentScrollArea = ({ challengeId }: CommentScrollAreaProps) => {
   const [comments, setComments] = useState<FirebaseCommentDocType[]>([]);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
 
   // TODO: limit and pagination for comments
   const collRef = collection(db, "comments");
@@ -28,6 +29,7 @@ const CommentScrollArea = ({ challengeId }: CommentScrollAreaProps) => {
     orderBy("createdAt", "asc")
   );
 
+  // data fetching from firebase
   useEffect(() => {
     if (!challengeId) return;
 
@@ -52,6 +54,16 @@ const CommentScrollArea = ({ challengeId }: CommentScrollAreaProps) => {
     };
   }, [challengeId]);
 
+  // auto scroll to bottom
+  useEffect(() => {
+    if (comments.length) {
+      scrollRef.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [comments.length]);
+
   return (
     <ScrollArea className="flex-1 flex flex-col w-full h-full space-y-5 mb-4">
       {comments.length ? (
@@ -64,6 +76,7 @@ const CommentScrollArea = ({ challengeId }: CommentScrollAreaProps) => {
           </p>
         </div>
       )}
+      <div ref={scrollRef} />
     </ScrollArea>
   );
 };
