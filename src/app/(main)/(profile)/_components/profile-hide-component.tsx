5dad7726d5b37a336118075c27fb61293fd76aa7
Kei-K23 <keiksl2301@gmail.com>
2024-06-13 21:19:27 +0630

    Add private and public switch button for user profile

--- src/app/(main)/(profile)/_components/profile-hide-component.tsx
+++ src/app/(main)/(profile)/_components/profile-hide-component.tsx
@@ -2,8 +2,9 @@
 
 import { toggleUserProfileView } from "@/actions/user-action";
 import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import React, { useEffect, useRef, useState, useTransition } from "react";
+
+import Switch from "react-switch";
+import React, { useState, useTransition } from "react";
 import { toast } from "sonner";
 
 type ProfileHideComponentProps = {
@@ -15,52 +16,33 @@ const ProfileHideComponent = ({
 }: ProfileHideComponentProps) => {
   const [pending, startTransition] = useTransition();
   const [isPrivate, setIsPrivate] = useState<boolean>(privateProfile);
-  const toggleRef = useRef<null | HTMLButtonElement>(null);
-  const [isClient, setIsClient] = useState(false);
 
-  const onClick = (value: string) => {
-    const isPrivate = value === "true" ? true : false;
-    console.log("here hit", isPrivate);
-
+  const handleOnChange = (value: boolean) => {
     startTransition(() => {
-      toggleUserProfileView(isPrivate)
+      toggleUserProfileView(value)
         .then(() => {
-          setIsPrivate(isPrivate);
+          setIsPrivate(value);
         })
-        .catch(() => toast.error("Something went wrong"));
+        .catch((e) => {
+          console.log(e);
+
+          toast.error("Something went wrong");
+        });
     });
   };
 
-  //! ERROR Here NEED TO HANDLE
-  useEffect(() => {
-    setIsClient(true);
-    if (toggleRef.current) {
-      console.log("hre");
-      console.log(toggleRef.current.ariaChecked);
-
-      toggleRef.current.ariaChecked = privateProfile ? "false" : "true";
-      console.log(toggleRef.current.ariaChecked);
-    }
-  }, [privateProfile]);
-
-  if (!isClient) {
-    return null;
-  }
-
   return (
-    <div className="flex items-center space-x-2">
-      <Switch
-        ref={toggleRef}
-        id="airplane-mode"
-        aria-checked={privateProfile ? "false" : "true"}
-        onClick={(e) => {
-          onClick(e.currentTarget.ariaChecked!);
-        }}
-        disabled={pending}
-      />
+    <div className="flex items-center space-x-2 mb-8">
       <Label htmlFor="airplane-mode">
         {isPrivate ? "Private Profile" : "Public Profile"}
       </Label>
+      <Switch
+        onChange={(checked) => handleOnChange(checked)}
+        disabled={pending}
+        checked={isPrivate}
+        checkedIcon={false}
+        uncheckedIcon={false}
+      />
     </div>
   );
 };
