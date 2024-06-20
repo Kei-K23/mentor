64b744190d765d36aa73c7d05ed2f1a6e3e296f1
Kei-K23 <keiksl2301@gmail.com>
2024-06-20 20:50:01 +0630

    Update release list

--- src/shared/release-list.ts
+++ src/shared/release-list.ts
@@ -1,5 +1,15 @@
 export const releaseList = [
     {
+        version: "v0.1.1-beta",
+        description: "Add profile viewers feature to know who see your profile. Add Private/Public profile toggle feature to hide or show publicly your profile informations",
+        features: [
+            "Profile viewers",
+            "Private/public profile",
+        ],
+        order: 2,
+        date: "2024-06-20"
+    },
+    {
         version: "v0.0.1-beta",
         description: "This is first and beta release for Mentor open-source LMS web applications. This release includes basic features and functionality for Mentor.",
         features: [
